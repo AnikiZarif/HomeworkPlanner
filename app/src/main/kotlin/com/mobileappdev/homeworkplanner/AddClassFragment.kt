e2a093525aae77b1b7516014461bffc722a94196
AnikiZarif <ismail.120@osu.edu>
2019-10-22 15:42:04 -0400

    Added fragment to login page but hideous very hideous. Need help

--- app/src/main/kotlin/com/mobileappdev/homeworkplanner/AddClassFragment.kt
+++ app/src/main/kotlin/com/mobileappdev/homeworkplanner/AddClassFragment.kt
@@ -19,7 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 
 class
-AddClassFragment: Fragment(), View.OnClickListener {
+AddClassFragment: Fragment() {
     private lateinit var mClassNameEditText: EditText
     private lateinit var mCreditHoursSpinner: Spinner
     private lateinit var mAddClassButton: Button
@@ -30,7 +30,7 @@ AddClassFragment: Fragment(), View.OnClickListener {
 
     private val TAG = AddClassFragment::class.java!!.getSimpleName()
     
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):View? {
         val v: View
         val rotation = activity?.windowManager?.defaultDisplay?.rotation
         v = if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
@@ -40,6 +40,7 @@ AddClassFragment: Fragment(), View.OnClickListener {
             inflater.inflate(R.layout.fragment_add_class, container, false)
         }
 
+        Log.d(TAG,"CLass ${v}")
         mClassNameEditText = v.findViewById(R.id.class_name_text)
         mCreditHoursSpinner = v.findViewById(R.id.credit_hours_spinner)
 
@@ -48,17 +49,24 @@ AddClassFragment: Fragment(), View.OnClickListener {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         mCreditHoursSpinner.adapter = adapter
 
+        val clickListener = View.OnClickListener {view ->
+            when (view.getId()) {
+                R.id.time_button -> showTimePickerDialog(v)
+                R.id.date_button -> showDatePickerDialog(v)
+                R.id.add_button -> addToFirestore()
+            }
+        }
         mClassTimeButton = v.findViewById(R.id.time_button)
-        mClassTimeButton.setOnClickListener(this)
+        mClassTimeButton.setOnClickListener(clickListener)
 
         mClassDateButton = v.findViewById(R.id.date_button)
-        mClassDateButton.setOnClickListener(this)
+        mClassDateButton.setOnClickListener(clickListener)
 
         mAddClassButton = v.findViewById(R.id.add_button)
-        mAddClassButton.setOnClickListener(this)
+        mAddClassButton.setOnClickListener(clickListener)
 
         mDeleteClassButton = v.findViewById(R.id.delete_button)
-        mDeleteClassButton.setOnClickListener(this)
+        mDeleteClassButton.setOnClickListener(clickListener)
 
         return v
     }
@@ -98,13 +106,14 @@ AddClassFragment: Fragment(), View.OnClickListener {
     }
     
   */
-
-    override fun onClick(view: View) {
+/*
+    override fun onClick() {
         when (view.id) {
             R.id.time_button -> showTimePickerDialog(view)
             R.id.date_button -> showDatePickerDialog(view)
             R.id.add_button -> addToFirestore()
         }
     }
+    */
 
 }
--- app/src/main/kotlin/com/mobileappdev/homeworkplanner/LoginUser.kt
+++ app/src/main/kotlin/com/mobileappdev/homeworkplanner/LoginUser.kt
@@ -22,11 +22,61 @@ import com.google.firebase.firestore.Query;
 
 class LoginUser: AppCompatActivity(){
     private val TAG = LoginUser::class.java!!.getSimpleName()
+    private lateinit var mUserNameText : EditText
+    private lateinit var mPasswordText : EditText
+    private lateinit var mResetButton : Button
+    private lateinit var mSubmitButton : Button
+    private lateinit var mNewUserButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?){
-        Log.d(TAG,"HERE OOOOOOOOOOOO")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_login)
+
+
+        val clickListener = View.OnClickListener {view ->
+            when (view.getId()) {
+                R.id.reset -> resetText()
+                R.id.new_user -> openFragment()
+                R.id.submit -> null
+            }
+        }
+
+        mUserNameText = findViewById(R.id.user_name)
+
+        mPasswordText = findViewById(R.id.password)
+
+        mResetButton = findViewById(R.id.reset)
+        mResetButton.setOnClickListener(clickListener)
+
+        mSubmitButton = findViewById(R.id.submit)
+        mSubmitButton.setOnClickListener(clickListener)
+
+        mNewUserButton = findViewById(R.id.new_user)
+        mNewUserButton.setOnClickListener(clickListener)
+        Log.d(TAG, "Login page created")
+
     }
+
+
+    fun resetText(){
+        mUserNameText.setText("")
+        mPasswordText.setText("")
+    }
+
+    fun openFragment(){
+        val fm = supportFragmentManager
+        var fragment = fm.findFragmentById(R.id.fragment_container)
+
+        Log.d(TAG,"HERE + ${fragment}")
+        if (fragment == null) {
+            fragment = AddClassFragment()
+            Log.d(TAG,"HEREyyyyyyyyyy")
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit()
+        }
+    }
+
+
 
 }
